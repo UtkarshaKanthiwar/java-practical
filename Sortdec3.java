9ae23973fb53eacdb6e141201609ea5279c28c3a
UtkarshaKanthiwar <95921742+UtkarshaKanthiwar@users.noreply.github.com>
2022-02-04 16:25:55 +0530

    Add files via upload

---
+++ Sortdec3.java
@@ -0,0 +1,12 @@
+package project;
+import java.util.Arrays;
+public class Sortdec3 {
+
+	public static void main(String[] args) {
+		String[] array= {"Mango","Apple","Grapes", "Papaya", "Pineapple", "Bannana","Orange"};
+		Arrays.sort(array);
+		System.out.println("Array elements in decending order: "+ Arrays.toString(array));
+
+	}
+
+}
