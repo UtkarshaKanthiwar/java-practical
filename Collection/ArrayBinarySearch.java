bea5dafe1ba654223256ba985224b05593cf3637
UtkarshaKanthiwar <95921742+UtkarshaKanthiwar@users.noreply.github.com>
2022-02-12 07:41:42 -0800

    Add files via upload

---
+++ Collection/ArrayBinarySearch.java
@@ -0,0 +1,18 @@
+package utkarsha;
+
+import java.util.Arrays;
+
+    class ArrayBinarySearch {
+    public static void main(String[] args) {
+		int arr[]= {10,20,30,40,50};
+		int key=30;
+		int result=Arrays.binarySearch(arr, key);
+		if(result<0)
+			System.out.println("Element is not found!");
+		else
+			System.out.println("Element is found at index:"+result);
+
+	}
+
+}
+// find by index value
